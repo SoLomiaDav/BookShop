0b674d67ee8370b7fa7955daefbf6b1145e6c336
SoLomiaDav <92159435+SoLomiaDav@users.noreply.github.com>
2022-06-12 19:57:05 +0300

    controller

---
+++ Shop/Shop/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Shop.Data.Interfaces;
+using Shop.Data.mocks;
+
+namespace Shop
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IAllBooks, MockBooks>();
+            services.AddTransient<IBooksCategory, MockCategory>();
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
